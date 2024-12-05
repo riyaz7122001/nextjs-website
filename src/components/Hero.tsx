b0f4b6f209dd5ff9e369397bc7c0f32751ab10b0
riyaz <riyaz.s@osplabs.com>
2024-12-05 23:26:07 +0530

    good stance for hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,10 @@ import GetStarted from "./GetStarted";
 const Hero = () => {
     return (
         <section id="home" className={`sm:py-16 py-6 flex md:flex-row flex-col text-white w-full`}>
-            <div className={`flex flex-col justify-center items-start flex-1 xl:px-0 sm:px-16 px-6 w-[50%]`}>
-                <div className={`flex flex-row items-center justify-around py-[6px] px-4 bg-discount-gradient text-discount-gradient rounded-[10px] mb-2 text-${styles.glass}`}>
-                    <Image src={"/icons/Discount.svg"} alt="discount" width={32} height={32} />
+            {/* Left Section */}
+            <div className="flex flex-col justify-center items-start flex-1 xl:px-0 sm:px-16 px-6">
+                <div className="flex flex-row items-center justify-around py-[6px] px-4 bg-discount-gradient text-discount-gradient rounded-[10px] mb-2">
+                    <Image src="/icons/Discount.svg" alt="discount" width={32} height={32} />
                     <p className={`${styles.paragraph}`}>
                         <span className="text-white">30% </span>
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">Discount For</span> {' '}
@@ -15,28 +16,31 @@ const Hero = () => {
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">Account</span>
                     </p>
                 </div>
-                <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row justify-between items-center w-full">
                     <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px]">
                         The Next <br className="sm:block hidden" />{" "}
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">Generation</span>
                         <br className="sm:block hidden" />
                     </h1>
-                    <div className="ss:flex md:mr-4 mr-0 text-white">
+                    <div className="ss:flex md:mr-4 mr-0">
                         <GetStarted />
                     </div>
                 </div>
 
-                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px] w-full">
+                <h1 className="flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px] w-full">
                     Payment Method.
                 </h1>
-                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.
                 </p>
             </div>
-            <div className="w-[50%]">
-                <img src="/icons/robot.png" className="w-[100%] h-[100%] relative z-[5]" alt="billing" />
+
+            {/* Right Section */}
+            <div className="flex-1 flex justify-center items-center relative">
+                <img src="/icons/robot.png" className="max-w-full max-h-full" alt="billing" />
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
