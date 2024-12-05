79d627f66601d30a2672b913538dbc0baf207bbd
riyaz <riyaz.s@osplabs.com>
2024-12-05 23:25:06 +0530

    Hero section changes

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,12 +1,27 @@
 import { arrowUp } from "@/constants";
 import styles from "@/styles/styles";
+import Image from "next/image";
 
 const GetStarted = () => {
     return (
-        <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full 
-        border-4 bg-white border-gradient-to-r from-white to-[#5CE1E6] 
-        cursor-pointer p-[2px]`}>
-            <div className="flex justify-center items-center flex-col w-[100%] h-[100%] rounded-full"></div>
+        <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full border-4 border-gradient-to-r 
+        from-white to-[#5CE1E6] cursor-pointer p-[2px] border-[#5CE1E6] mr-12`}>
+            <div className="flex justify-center items-center bg-primary flex-col w-[100%] h-[100%] rounded-full 
+            border-gradient-to-r from-white to-[#5CE1E6]">
+                <div className="flex justify-center items-start flex-row">
+                    <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">
+                            Get
+                        </span>
+                    </p>
+                    <Image src={arrowUp} alt="arrow-up" width={23} height={23} />
+                </div>
+                <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">
+                        Started
+                    </span>
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,8 @@ import GetStarted from "./GetStarted";
 
 const Hero = () => {
     return (
-        <section
-            id="home"
-            className={`sm:py-16 py-6 flex md:flex-row flex-col text-white`}>
-            <div className={`flex flex-col justify-center items-start flex-1 xl:px-0 sm:px-16 px-6`}>
+        <section id="home" className={`sm:py-16 py-6 flex md:flex-row flex-col text-white w-full`}>
+            <div className={`flex flex-col justify-center items-start flex-1 xl:px-0 sm:px-16 px-6 w-[50%]`}>
                 <div className={`flex flex-row items-center justify-around py-[6px] px-4 bg-discount-gradient text-discount-gradient rounded-[10px] mb-2 text-${styles.glass}`}>
                     <Image src={"/icons/Discount.svg"} alt="discount" width={32} height={32} />
                     <p className={`${styles.paragraph}`}>
@@ -21,12 +19,21 @@ const Hero = () => {
                     <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px]">
                         The Next <br className="sm:block hidden" />{" "}
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#5CE1E6]">Generation</span>
-                        <br className="sm:block hidden" /> Payment Method.
+                        <br className="sm:block hidden" />
                     </h1>
                     <div className="ss:flex md:mr-4 mr-0 text-white">
                         <GetStarted />
                     </div>
                 </div>
+
+                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px] w-full">
+                    Payment Method.
+                </h1>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.
+                </p>
+            </div>
+            <div className="w-[50%]">
+                <img src="/icons/robot.png" className="w-[100%] h-[100%] relative z-[5]" alt="billing" />
             </div>
         </section>
     )
